54ab3defcc4d6a9f292d545ea8f54a5bcf0bb76d
Kumail Raza <kumailraza260@gmail.com>
2023-09-28 21:32:37 +0500

    lastUpdate

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
         ),
       home: Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter Demo API 2 "),
+        title: const Text(" Flutter Demo API 2 "),
       ),
       body: const HomePage(),
       ),
